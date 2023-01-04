dc9b7ba61013a336beb585d2af27e687d25064ca
SAMPATH KUMAR P <81365034+SampathKumarP101@users.noreply.github.com>
2023-01-04 14:29:19 +0000

    #package.json fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private usersCollection: AngularFirestoreCollection<IUser>;
+  public isAuthenticated$: Observable<boolean>;
 
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) { }
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore)
+  {
+    this.usersCollection = db.collection('users');
+    this.isAuthenticated$ = auth.user.pipe(
+      map((user: any) => !!user)
+      )
+  }
 
   public async createUser(userData: IUser) {
+    if(!userData.password)
+    {
+      throw new Error("Password not Provided!")
+    }
     const userCred = await this.auth.createUserWithEmailAndPassword(
       userData.email as string, userData.password as string
     )
-    await this.db.collection('users').add({
+    await this.usersCollection.doc(userCred.user?.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber
+    })
+
+    await userCred.user?.updateProfile({
+      displayName: userData.name
     })
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
 
 if (environment.production) {
   enableProdMode();
 }
+firebase.initializeApp(environment.firebase)
+let appInint = false
+firebase.auth().onAuthStateChanged(() => {
+  if(!appInint) {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+  }
+appInint = true;
+})
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
